b58f10c8699ea34364cac13e841ee675513b57a2
Coder731 <stephenobrien1@gmail.com>
2021-08-15 01:14:48 +0000

    Update main2 py

--- mian2.py
+++ mian2.py
@@ -1,17 +1,4 @@
 users = ['anna','chris','brian']
 for user in range(len(users)):
-
-    a=users[user]
-    print("a="+a)
-
-    b=users[user].capitalize()
-    print("b="+b)
-
-    c=str(user)
-    print("c="+c)
-
-    users[user]=b
-
-
-
+    users[user]=users[user].capitalize()
 print(users)
